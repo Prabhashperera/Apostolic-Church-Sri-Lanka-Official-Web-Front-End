caeff66d9e636649ffd5c29d7d93002208b74208
acsl-srilanka <apostolicmoratuwa123@gmail.com>
2026-09-05 14:28:09 +0530

    Changes: Restrcure v3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import Hero from "./Hero";
+
 export default function Home() {
   return (
     <div className="bg-white">
 
       <div className="px-4 py-6 mx-auto space-y-12 md:space-y-16 md:px-8 md:py-10 max-w-7xl">
-
+        <Hero />
       </div>
     </div>
   );
